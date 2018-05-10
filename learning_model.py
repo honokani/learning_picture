df741872cd58b9b350eef07423a23d84fb70323a
show <honokani.kaoriga@gmail.com>
2018-05-10 07:06:51 +0000

    UPDATE enview tensorboard, FIX train_rate

--- learning_model.py
+++ learning_model.py
@@ -1,6 +1,7 @@
 import cv2, os, re, glob, math
 import numpy as np
 import tensorflow as tf
+import tensorflow.python.platform
 from libs import data_input as data_in
 
 COMMON_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -24,48 +25,50 @@ def getInputDatas():
     data_x = np.array(ds, dtype="float") / 255.0
     return data_x, data_l
 
+
 def main():
-    def makeBiasVal(shape):
-        b = tf.constant(0.1, shape=shape)
-        return tf.Variable(b)
-
-    def makeWeightVal(shape):
-        # initialize weight by gauusian
-        w = tf.truncated_normal(shape, stddev=0.1)
-        return tf.Variable(w)
-
-    def makeConvLayer(ch_in, ch_out, sz):
-        # define stride size
-        UNUSED_PARAM      = 1
-        shift_span       = [UNUSED_PARAM, 1, 1, UNUSED_PARAM]
-        conv_window_size = [sz, sz, ch_in, ch_out]
-        # define size of w and d
-        w = makeWeightVal(conv_window_size)
-        b = makeBiasVal([ch_out])
-        return lambda x: tf.nn.relu( b + tf.nn.conv2d(x, w, shift_span, padding='SAME') )
-
-    def makePoolLayer(sz):
-        UNUSED_PARAM      = 1
-        pool_window_size = [UNUSED_PARAM, sz, sz, UNUSED_PARAM]
-        shift_span       = [UNUSED_PARAM, sz, sz, UNUSED_PARAM]
-        return lambda x: tf.nn.max_pool(x, ksize=pool_window_size, strides=shift_span, padding='SAME')
-
-    def makeFullyConnectedLayer(in_sz, out_sz, f):
-        w = makeWeightVal([in_sz, out_sz])
-        b = makeBiasVal([out_sz])
-        return lambda x: f( b + tf.matmul(tf.reshape(x, [-1, in_sz]), w) )
-
-    def makeFullyConnectedLayerWithRelu(in_sz, out_sz):
-        return makeFullyConnectedLayer(in_sz, out_sz, tf.nn.relu)
-
-    def makeFullyConnectedLayerWithSoftmax(in_sz, out_sz):
-        return makeFullyConnectedLayer(in_sz, out_sz, tf.nn.softmax)
-
-    def makeDropoutLayer(dr_ratio):
-        return lambda x: tf.nn.dropout(x, 1-dr_ratio)
 
     # Build CNN Model
     def buildModel(ph_x, drop_ratio, x_params, label_num):
+        def makeBiasVal(shape):
+            b = tf.constant(0.1, shape=shape)
+            return tf.Variable(b)
+
+        def makeWeightVal(shape):
+            # initialize weight by gauusian
+            w = tf.truncated_normal(shape, stddev=0.1)
+            return tf.Variable(w)
+
+        def makeConvLayer(ch_in, ch_out, sz):
+            # define stride size
+            UNUSED_PARAM      = 1
+            shift_span       = [UNUSED_PARAM, 1, 1, UNUSED_PARAM]
+            conv_window_size = [sz, sz, ch_in, ch_out]
+            # define size of w and d
+            w = makeWeightVal(conv_window_size)
+            b = makeBiasVal([ch_out])
+            return lambda x: tf.nn.relu( b + tf.nn.conv2d(x, w, shift_span, padding='SAME') )
+
+        def makePoolLayer(sz):
+            UNUSED_PARAM      = 1
+            pool_window_size = [UNUSED_PARAM, sz, sz, UNUSED_PARAM]
+            shift_span       = [UNUSED_PARAM, sz, sz, UNUSED_PARAM]
+            return lambda x: tf.nn.max_pool(x, ksize=pool_window_size, strides=shift_span, padding='SAME')
+
+        def makeFullyConnectedLayer(in_sz, out_sz, f):
+            w = makeWeightVal([in_sz, out_sz])
+            b = makeBiasVal([out_sz])
+            return lambda x: f( b + tf.matmul(tf.reshape(x, [-1, in_sz]), w) )
+
+        def makeFullyConnectedLayerWithRelu(in_sz, out_sz):
+            return makeFullyConnectedLayer(in_sz, out_sz, tf.nn.relu)
+
+        def makeFullyConnectedLayerWithSoftmax(in_sz, out_sz):
+            return makeFullyConnectedLayer(in_sz, out_sz, tf.nn.softmax)
+
+        def makeDropoutLayer(dr_ratio):
+            return lambda x: tf.nn.dropout(x, 1-dr_ratio)
+
         # get size of flatten x and label
         x_w, x_h, x_ch = x_params
         pic_flat_size = x_w * x_h
@@ -76,41 +79,54 @@ def main():
         # 1st conv-pool layer set
         h1_out_ch = 32
         h1_w_size = 5
-        h1_c = makeConvLayer(x_ch, h1_out_ch, h1_w_size)(x_in)
+
+        with tf.name_scope('conv1') as scope:
+            h1_c = makeConvLayer(x_ch, h1_out_ch, h1_w_size)(x_in)
         h1_p_size = 2
-        h1_p = makePoolLayer(h1_p_size)(h1_c)
+        with tf.name_scope('pool1') as scope:
+            h1_p = makePoolLayer(h1_p_size)(h1_c)
         # 2nd conv-pool layer set
         h2_out_ch = 64
         h2_w_size = 5
-        h2_c = makeConvLayer(h1_out_ch, h2_out_ch, h2_w_size)(h1_p)
+        with tf.name_scope('conv2') as scope:
+            h2_c = makeConvLayer(h1_out_ch, h2_out_ch, h2_w_size)(h1_p)
         h2_p_size = 2
-        h2_p = makePoolLayer(h2_p_size)(h2_c)
+        with tf.name_scope('pool2') as scope:
+            h2_p = makePoolLayer(h2_p_size)(h2_c)
         # full connected layer and drop layer
         fc1_pic_size = int(pic_flat_size / math.pow(h1_p_size * h2_p_size, 2))
         fc1_pic_flatten = fc1_pic_size * h2_out_ch
         fc1_layer_len = 1024
-        fc1_out = makeFullyConnectedLayerWithRelu(fc1_pic_flatten, fc1_layer_len)(h2_p)
-        fc1_rest = makeDropoutLayer(drop_ratio)(fc1_out)
+        with tf.name_scope('fullyconn') as scope:
+            fc1_out = makeFullyConnectedLayerWithRelu(fc1_pic_flatten, fc1_layer_len)(h2_p)
+        with tf.name_scope('drop') as scope:
+            fc1_rest = makeDropoutLayer(drop_ratio)(fc1_out)
         # normalization
         fc2_layer_len = label_num
-        fc2_out = makeFullyConnectedLayerWithSoftmax(fc1_layer_len, fc2_layer_len)(fc1_rest)
+        with tf.name_scope('softmax') as scope:
+            fc2_out = makeFullyConnectedLayerWithSoftmax(fc1_layer_len, fc2_layer_len)(fc1_rest)
+        # 
 
         return fc2_out
 
     def defineLoss(result, correct):
         # culc cross entropy
-        return -tf.reduce_sum( correct * tf.log(result) )
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=correct, logits=result)
+        loss = tf.reduce_mean(cross_entropy)
+        tf.summary.scalar("loss_value", loss)
+        return tf.reduce_mean(loss)
 
     def minimizeLoss(loss):
-        train_ratio = 0.2
+        train_ratio = 0.0000002
         return tf.train.AdamOptimizer( train_ratio ).minimize( loss )
 
     def checkAccuracy(result, correct):
         prediction = tf.equal(tf.argmax(result, 1), tf.argmax(correct, 1))
         accuracy = tf.reduce_mean(tf.cast(prediction, "float"))
+        tf.summary.scalar("accuracy", accuracy)
         return accuracy
 
-    def runLearning(sess, ops, itors, inputs, train_ratio):
+    def runLearning(ops, itors, inputs, valid_ratio):
         e = np.array([])
         npSplit=lambda n,t:(e,t)if(len(t)<1 or n<1)else((t,e)if(len(t)<n)else(t[:n],t[n:]))
 
@@ -118,10 +134,16 @@ def main():
         training_step, check_step = ops[0], ops[1]
         epoch_sz, minibatch_sz = itors[0], itors[1]
 
-        # pick inputs for accuracy test
+        # count train data size for accuracy test
         xs, ys = inputs[0], inputs[1]
         input_sz = len(ys)
-        test_sz  = int(input_sz * train_ratio)
+        test_sz  = int(input_sz * valid_ratio)
+        if(test_sz<1):
+            test_sz = 1
+        elif(input_sz<test_sz):
+            test_sz = input_sz -1
+
+        # split test_data and train_data
         trains_num = np.random.permutation(input_sz)
         tgt_to_accuracy, tgt_to_train = npSplit(test_sz, trains_num)
         test_x   = xs[tgt_to_accuracy]
@@ -130,21 +152,28 @@ def main():
         train_y  = ys[tgt_to_train]
         train_sz = input_sz - test_sz
 
+        # TensorBoardで表示する値の設定
+        summary_op = tf.summary.merge_all()
+        summary_writer = tf.summary.FileWriter(COMMON_DIR_PATH, sess.graph)
+
         # run epoch-size time
         for ep in range(epoch_sz):
             rest_tgt = np.random.permutation(train_sz)
+            # rest_tgt = np.array(list(range(train_sz)))
             while(0 < rest_tgt.size):
                 tgt_to_pick, rest_tgt = npSplit(minibatch_sz, rest_tgt)
                 batch_x = train_x[tgt_to_pick]
                 batch_y = train_y[tgt_to_pick]
-                sess.run(training_step, feed_dict={ph_x:batch_x, ph_y:batch_y, drop_ratio:0.0})
-            if(ep%10 == 0):
-                train=sess.run(check_step, feed_dict={ph_x:batch_x, ph_y:batch_y, drop_ratio:0.0})
-                acc=sess.run(check_step, feed_dict={ph_x:test_x, ph_y:test_y, drop_ratio:0.0})
-                print("ep: {}, train: {}, accuracy: {}".format(ep, train, acc))
-
-        # return result
-        return sess
+                sess.run(training_step, feed_dict={ph_x:batch_x, ph_y:batch_y, drop_ratio:0.1})
+                if(len(rest_tgt)<minibatch_sz):
+                    break
+            if(ep%1 == 0):
+                noDrop = 0.0
+                trn=sess.run(check_step,feed_dict={ph_x:batch_x, ph_y:batch_y, drop_ratio:noDrop})
+                acc=sess.run(check_step,feed_dict={ph_x:test_x, ph_y:test_y, drop_ratio:noDrop})
+                sums=sess.run(summary_op,feed_dict={ph_x:test_x, ph_y:test_y, drop_ratio:noDrop})
+                summary_writer.add_summary(sums,ep)
+                print("ep: {}, train: {}, accuracy: {}".format(ep, trn, acc))
 
 
     # get input datas
@@ -163,7 +192,7 @@ def main():
         # define place holder
         ph_x = tf.placeholder(tf.float32, shape=[None, x_w, x_h, 1, x_ch])
         ph_y = tf.placeholder(tf.float32, shape=[None, label_num])
-        # (1-dropout ratio) placeholder
+        # (1-keep ratio) placeholder
         drop_ratio = tf.placeholder(tf.float32)
 
         # build cnn model, get placeholder, and result logits
@@ -175,22 +204,21 @@ def main():
         # check accuracy
         check_step = checkAccuracy(result_logits, ph_y)
 
-        # ready session
-        sess  = tf.Session()
+        # ready saver
         saver = tf.train.Saver()
         # initialize session
+        sess = tf.Session()
         sess.run(tf.global_variables_initializer())
 
         # do learn
-        total_x_num = ph_x.shape[0]
-        epoch_sz = 100
+        epoch_sz = 1000
         minibatch_sz = 100
-        sess = runLearning( sess
-                          , (training_step, check_step)
-                          , (epoch_sz, minibatch_sz)
-                          , (in_xs, in_ys)
-                          , 0.2
-                          )
+        runLearning( (training_step, check_step)
+                   , (epoch_sz, minibatch_sz)
+                   , (in_xs, in_ys)
+                   , 0.1
+                   )
+
         # save model
         model_name = "model.ckpt"
         model_path = os.path.join(COMMON_DIR_PATH, model_name)
